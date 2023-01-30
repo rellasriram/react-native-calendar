7476615f52980b591992736c4ece8f4ac069a529
Chow, Andrew Michael <andrewchow@gatech.edu>
2023-01-29 21:59:07 -0500

    Functional Front End
    Added: Event Name text box, event date/calendar functionality, and create event button.
    
    To-dos:
    -Save event name and date and send as API
    -“Create Event” Button is not functional (needs to check if user filled out both inputs AND needs to send API AND needs to clear inputs)
    -“Event Name” text box is not focused
    -Make UI look nicer

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,14 @@
-import { useNavigation } from '@react-navigation/core'
-import React from 'react'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { NavigationHelpersContext, useNavigation } from '@react-navigation/core'
+import React, { useRef, useState } from 'react'
+import { StyleSheet, Text, TouchableOpacity, View, KeyboardAvoidingView, TextInput } from 'react-native'
 import { auth } from '../firebase'
+import DatePicker from '@react-native-community/datetimepicker';
 
 const HomeScreen = () => {
+
+  const [eventName, setEventName] = useState('');
+  const [date, setDate] = useState(new Date());
+
   const navigation = useNavigation()
 
   const handleSignOut = () => {
@@ -15,26 +20,92 @@ const HomeScreen = () => {
       .catch(error => alert(error.message))
   }
 
+  const handlePageChange = () => {
+    navigation.replace("Event")
+  }
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setDate(currentDate);
+
+    let tempDate = new Date(currentDate);
+    let fDate = tempDate.getDate() + '/' + (tempDate.getMonth() + 1) + '/' + tempDate.getFullYear();
+
+    //console.log(fDate)
+  }
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Email: {auth.currentUser?.email}</Text>
-      <TouchableOpacity
-        onPress={handleSignOut}
-        style={styles.button}
-      >
-        <Text style={styles.buttonText}>Sign out</Text>
-      </TouchableOpacity>
-    </View>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior="padding"
+    >
+      <View style={styles.container}>
+        <Text>Email: {auth.currentUser?.email}</Text>    
+
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="Event Name"
+            value={eventName}
+            onChangeText={text => setEventName(text)}
+            style={styles.input}
+            autoFocus
+          />
+         </View>
+
+        <Text>Choose Event Date: </Text>
+
+        {
+          <DatePicker
+            testID = 'datepicker'
+            value = {date}
+            mode = 'date'
+            display = 'default'
+            onChange = {onChange}
+          />
+        }
+        
+        <TouchableOpacity
+          onPress={handleSignOut} //fix this
+          style={styles.button}
+        >
+        <Text style={styles.buttonText}>Create Event</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={handleSignOut}
+          style={[styles.button, styles.buttonOutline]}
+        >
+          <Text style={styles.buttonOutlineText}>Sign out</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   )
 }
 
 export default HomeScreen
 
 const styles = StyleSheet.create({
+  
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  inputContainer: {
+    width: '80%',
+    marginTop: 25
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
   },
    button: {
     backgroundColor: '#0782F9',
@@ -42,10 +113,21 @@ const styles = StyleSheet.create({
     padding: 15,
     borderRadius: 10,
     alignItems: 'center',
-    marginTop: 40,
+    marginTop: 25,
   },
   buttonText: {
     color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  buttonOutline: {
+    backgroundColor: 'white',
+    marginTop: 25,
+    borderColor: '#0782F9',
+    borderWidth: 2,
+  },
+  buttonOutlineText: {
+    color: '#0782F9',
     fontWeight: '700',
     fontSize: 16,
   },
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -43,40 +43,40 @@ const LoginScreen = () => {
 
   return (
     <KeyboardAvoidingView
-        style={styles.container}
-        behavior="padding"
+      style={styles.container}
+      behavior="padding"
     >
-        <View style={styles.inputContainer}>
-            <TextInput
-                placeholder="Email"
-                value={email}
-                onChangeText={text => setEmail(text)}
-                style={styles.input}
-            />
-            <TextInput
-                placeholder="Password"
-                value={password}
-                onChangeText={text => setPassword(text)}
-                style={styles.input}
-                secureTextEntry
-            />
-        </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={text => setEmail(text)}
+          style={styles.input}
+          />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={text => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
 
-        <View style={styles.buttonContainer}>
-            <TouchableOpacity
-                onPress={handleLogin}
-                style={styles.button}
-            >
-                <Text style={styles.buttonText}>Login</Text>
-            </TouchableOpacity>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={handleLogin}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
 
-            <TouchableOpacity
-                onPress={handleSignUp}
-                style={[styles.button, styles.buttonOutline]}
-            >
-                <Text style={styles.buttonOutlineText}>Register</Text>
-            </TouchableOpacity>
-        </View>
+        <TouchableOpacity
+          onPress={handleSignUp}
+          style={[styles.button, styles.buttonOutline]}
+        >
+        <Text style={styles.buttonOutlineText}>Register</Text>
+      </TouchableOpacity>
+      </View>
     </KeyboardAvoidingView>
   )
 }
@@ -88,44 +88,44 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-    },
+      },
     inputContainer: {
         width: '80%'
       },
-      input: {
-        backgroundColor: 'white',
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-        borderRadius: 10,
-        marginTop: 5,
-      },
-      buttonContainer: {
-        width: '60%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 40,
-      },
-      button: {
-        backgroundColor: '#0782F9',
-        width: '100%',
-        padding: 15,
-        borderRadius: 10,
-        alignItems: 'center',
-      },
-      buttonText: {
-        color: 'white',
-        fontWeight: '700',
-        fontSize: 16,
-      },
-      buttonOutline: {
-        backgroundColor: 'white',
-        marginTop: 5,
-        borderColor: '#0782F9',
-        borderWidth: 2,
-      },
-      buttonOutlineText: {
-        color: '#0782F9',
-        fontWeight: '700',
-        fontSize: 16,
-      },
+    input: {
+      backgroundColor: 'white',
+      paddingHorizontal: 15,
+      paddingVertical: 10,
+      borderRadius: 10,
+      marginTop: 5,
+    },
+    buttonContainer: {
+      width: '60%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 40,
+    },
+    button: {
+      backgroundColor: '#0782F9',
+      width: '100%',
+      padding: 15,
+      borderRadius: 10,
+      alignItems: 'center',
+    },
+    buttonText: {
+      color: 'white',
+      fontWeight: '700',
+      fontSize: 16,
+    },
+    buttonOutline: {
+      backgroundColor: 'white',
+      marginTop: 5,
+      borderColor: '#0782F9',
+      borderWidth: 2,
+    },
+    buttonOutlineText: {
+      color: '#0782F9',
+      fontWeight: '700',
+      fontSize: 16,
+    },
 })
